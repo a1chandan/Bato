4a21ecd090c3f262ccd377dd0d2bf5bee1c98f84
a1chandan <a1chandan@gmail.com>
2024-12-14 12:34:17 +0545

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ var drawnItems = new L.FeatureGroup();
 map.addLayer(drawnItems);
 
 var drawControl = new L.Control.Draw({
-   position: 'bottomleft'
+   position: 'bottomleft',
     draw: {
         polyline: {
             shapeOptions: {
